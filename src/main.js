69d9704b8305343b0984759228b4036608f25dec
yang tao <yangtao11242012@163.com>
2020-12-09 18:50:29 +0800

    rr

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
+import Vue from 'vue'        
 // 加载element-ui
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
